eddfa998f72134a97ff62ac47acfcb25dc653066
SanLi <2689170096@qq.com>
2019-01-17 13:34:04 +0800

    添加超时跳转登录页面

--- smallbun-fast-modules/src/main/resources/static/common/js/common.js
+++ smallbun-fast-modules/src/main/resources/static/common/js/common.js
@@ -625,7 +625,7 @@
             addTab: function (id, title, url) {
                 addTab({id: id, title: title, close: true, url: url})
             },
-            //添加
+            //添加tab页
             addTab: function () {
                 var url = $.common.isEmpty(id) ? $.table._option.createUrl : $.table._option.createUrl.replace("{id}", id);
                 $.modal.addTab("_tab" + Math.random().toString(36).substring(2), "添加" + $.table._option.modalName, url);
