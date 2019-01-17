e3be61d42bbc5764e6171e278a5554af3d51b7a5
SanLi <2689170096@qq.com>
2019-01-17 16:11:56 +0800

    添加

--- smallbun-fast-modules/src/main/resources/static/common/js/common.js
+++ smallbun-fast-modules/src/main/resources/static/common/js/common.js
@@ -550,7 +550,7 @@
             ,
             //添加，以tab页展现
             addTab: function (id) {
-                var url = $.common.isEmpty(id) ? $.table._option.createUrl : $.table._option.createUrl.replace("{id}", id);
+                var url = $.common.isEmpty(id) ? $.table._option.createUrl.replace("{id}", '') : $.table._option.createUrl.replace("{id}", id);
                 $.modal.openTab("_tab" + Math.random().toString(36).substring(2), "添加" + $.table._option.modalName, url);
             },
             // 修改信息
