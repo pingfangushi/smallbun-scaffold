e007299d12f408d957aa739c64c766b97af59c38
SanLi <2689170096@qq.com>
2019-01-20 10:16:15 +0800

    完成图标选择

--- smallbun-fast-modules/src/main/resources/static/modules/manage/js/icons.js
+++ smallbun-fast-modules/src/main/resources/static/modules/manage/js/icons.js
@@ -5,24 +5,41 @@ $(function () {
      * fa-icons 选中
      */
     $('#fa-icons').find("i").parent().on("click", function () {
-        //移除style
-        $('#glyphicons').find("li").removeAttr("style");
-        $('#fa-icons').find("i").parent().removeAttr("style");
+        remIconSel();
         //设置值
         $('#icon').val($(this).find('i').attr('class').trim());
         //给当前选中元素添加style样式
-        $(this).css({"background-color": "#479bff", "color": "#ffffff", "cursor": "pointer"});
+        $(this).css({"background-color": "#4F8EF7", "color": "#ffffff", "cursor": "pointer"});
     });
     /**
      * glyphicons 选中
      */
     $('#glyphicons').find("li").on("click", function () {
-        //移除style
-        $('#fa-icons').find("i").parent().removeAttr("style");
-        $('#glyphicons').find("li").removeAttr("style");
+        remIconSel();
         //设置值
         $('#icon').val($(this).find('span').eq(0).attr('class').trim());
         //给当前选中元素添加style样式
-        $(this).css({"background-color": "#479bff", "color": "#ffffff", "cursor": "pointer"});
+        $(this).css({"background-color": "#4F8EF7", "color": "#ffffff", "cursor": "pointer"});
     });
+    /**
+     * icons 选中
+     */
+    $('.icons').find("li").on("click", function () {
+        remIconSel();
+        //设置值
+        $('#icon').val($(this).attr('class').trim());
+        //给当前选中元素添加style样式
+        console.log($(this));
+        $(this).css({"background-color": "#4F8EF7", "color": "#ffffff", "cursor": "pointer"});
+    });
+
+    /**
+     * 移除图标选中样式
+     */
+    function remIconSel() {
+        //移除style
+        $('#fa-icons').find("i").parent().removeAttr("style");
+        $('#glyphicons').find("li").removeAttr("style");
+        $('.icons').find("li").removeAttr("style");
+    }
 });
