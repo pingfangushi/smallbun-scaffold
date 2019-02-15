07b2233366227917d2d01805a8fc57ec50b22d0a
SanLi <2689170096@qq.com>
2019-02-15 17:56:13 +0800

    编写通知公告，完善页面

--- smallbun-fast-modules/src/main/resources/static/modules/manage/js/notify/notify_form.js
+++ smallbun-fast-modules/src/main/resources/static/modules/manage/js/notify/notify_form.js
@@ -23,4 +23,100 @@
 
 //实例化编辑器
 //建议使用工厂方法getEditor创建和引用编辑器实例，如果在某个闭包下引用该编辑器，直接调用UE.getEditor('editor')就能拿到相关的实例
-var ue = UE.getEditor('content');
+var editor = UE.getEditor('content', {
+    autoHeight: false,
+    initialFrameHeight: 200,
+    initialFrameWidth: '100%',
+    toolbars: [
+        [
+            'undo', //撤销
+            'redo', //重做
+            'bold', //加粗
+            'indent', //首行缩进
+            'italic', //斜体
+            'underline', //下划线
+            'strikethrough', //删除线
+            'subscript', //下标
+            'fontborder', //字符边框
+            'superscript', //上标
+            'formatmatch', //格式刷
+            'blockquote', //引用
+            'pasteplain', //纯文本粘贴模式
+            'selectall', //全选
+            'preview', //预览
+            'horizontal', //分隔线
+            'removeformat', //清除格式
+            'time', //时间
+            'date', //日期
+            'unlink', //取消链接
+            'insertrow', //前插入行
+            'insertcol', //前插入列
+            'mergeright', //右合并单元格
+            'mergedown', //下合并单元格
+            'deleterow', //删除行
+            'deletecol', //删除列
+            'splittorows', //拆分成行
+            'splittocols', //拆分成列
+            'splittocells', //完全拆分单元格
+            'deletecaption', //删除表格标题
+            'inserttitle', //插入标题
+            'mergecells', //合并多个单元格
+            'deletetable', //删除表格
+            'cleardoc', //清空文档
+            'insertparagraphbeforetable', //"表格前插入行"
+            'fontfamily', //字体
+            'fontsize', //字号
+            'paragraph', //段落格式
+            'simpleupload', //单图上传
+            'insertimage', //多图上传
+            'edittable', //表格属性
+            'edittd', //单元格属性
+            'link', //超链接
+            'emotion', //表情
+            'spechars', //特殊字符
+            'searchreplace', //查询替换
+            'insertvideo', //视频
+            'justifyleft', //居左对齐
+            'justifyright', //居右对齐
+            'justifycenter', //居中对齐
+            'justifyjustify', //两端对齐
+            'forecolor', //字体颜色
+            'backcolor', //背景色
+            'insertorderedlist', //有序列表
+            'insertunorderedlist', //无序列表
+            'directionalityltr', //从左向右输入
+            'directionalityrtl', //从右向左输入
+            'rowspacingtop', //段前距
+            'rowspacingbottom', //段后距
+            'pagebreak', //分页
+            'imagenone', //默认
+            'imageleft', //左浮动
+            'imageright', //右浮动
+            'attachment', //附件
+            'imagecenter', //居中
+            'lineheight', //行间距
+            'edittip ', //编辑提示
+            'customstyle', //自定义标题
+            'autotypeset', //自动排版
+            'tolowercase', //字母小写
+            'background', //背景
+            'inserttable', //插入表格
+            'drafts', // 从草稿箱加载
+            'charts' // 图表
+        ]
+    ]
+});
+//加载完成展示
+editor.addListener('ready', function (editor) {
+    $('#content_row').show();
+});
+
+/**
+ * 提交事件
+ */
+function doSubmit() {
+    // 手动触发校验代码
+    if ($('.form-horizontal').valid()) {
+        $.operate.saveCurrentTabPage($('.form-horizontal').attr('action'), $('.form-horizontal').serializeArray());
+    }
+}
