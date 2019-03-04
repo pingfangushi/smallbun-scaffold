702888c4ccfc2b6487be687832595008ab9bfa18
SanLi <2689170096@qq.com>
2019-03-04 17:26:46 +0800

    新增导入导出，编写中

--- smallbun-fast-modules/src/main/resources/static/modules/manage/js/dict/dict_type_list.js
+++ smallbun-fast-modules/src/main/resources/static/modules/manage/js/dict/dict_type_list.js
@@ -28,7 +28,7 @@ $(function () {
         updateUrl: contextPath + "dict/type/form/{id}",
         removeUrl: contextPath + "dict/type/removeById",
         batRemoveUrl: contextPath + "dict/type/removeByIds",
-        exportUrl: contextPath + "/export",
+        exportUrl: contextPath + "dict/type/export",
         sortName: "gmtCreate",
         sortOrder: "desc",
         modalName: "字典",
@@ -42,16 +42,17 @@ $(function () {
             {field: 'typeName', title: '字典名称', formatter: $.table.view, sortable: true},
             {field: 'typeCode', title: '字典类型', sortable: true},
             {field: 'gmtModified', title: '更新时间', sortable: true},
-            {title: '操作',align: 'center', width: 50, visible: true, formatter: function (value, row, index) {
+            {
+                title: '操作', align: 'center', width: 50, visible: true, formatter: function (value, row, index) {
                     var actions = [];
-                    actions.push('<div class="btn-group"><button type="button" class="btn ibtn-primary btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-cog"></i>&nbsp;<span class="fa fa-chevron-down"></span></button>'+
-                                    '<ul class="dropdown-menu" role="menu">'+
-                                        '<li><a href="#" onclick="$.operate.view(\'' + row.id + '\',\'\')"><i class="fa fa-search-plus"></i>查看</a></li>'+
-                                        '<li><a href="#" onclick="$.operate.edit(\'' + row.id + '\')"><i class="fa fa-edit"></i>修改</a></li>'+
-                                        '<li><a href="#" onclick="$.operate.remove(\'' + row.id + '\')"><i class="fa fa-trash"></i>删除</a></li>'+
-                                        '<li><a href="#" onclick="$.modal.openTab(\'sys_dict_type' + row.id + '\', \'字典值\', \'dict/value?sysDictType.id=' + row.id + '\')"><i class="fa fa-plus"></i>字典值</a></li>'+
-                                    '</ul>'+
-                                '</div>');
+                    actions.push('<div class="btn-group"><button type="button" class="btn ibtn-primary btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-cog"></i>&nbsp;<span class="fa fa-chevron-down"></span></button>' +
+                        '<ul class="dropdown-menu" role="menu">' +
+                        '<li><a href="#" onclick="$.operate.view(\'' + row.id + '\',\'\')"><i class="fa fa-search-plus"></i>查看</a></li>' +
+                        '<li><a href="#" onclick="$.operate.edit(\'' + row.id + '\')"><i class="fa fa-edit"></i>修改</a></li>' +
+                        '<li><a href="#" onclick="$.operate.remove(\'' + row.id + '\')"><i class="fa fa-trash"></i>删除</a></li>' +
+                        '<li><a href="#" onclick="$.modal.openTab(\'sys_dict_type' + row.id + '\', \'字典值\', \'dict/value?sysDictType.id=' + row.id + '\')"><i class="fa fa-plus"></i>字典值</a></li>' +
+                        '</ul>' +
+                        '</div>');
                     return actions.join('');
                 }
             }]
