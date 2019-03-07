36010f4efa40f3189f6541e75484fe4f0f867e04
SanLi <2689170096@qq.com>
2019-03-07 15:16:42 +0800

    修复 https://gitee.com/leshalv/smallbun/issues/ISIXM TreeGrid 角色权限授权后登录BUG

--- smallbun-fast-modules/src/main/java/org/smallbun/fast/manage/user/service/impl/SysUserServiceImpl.java
+++ smallbun-fast-modules/src/main/java/org/smallbun/fast/manage/user/service/impl/SysUserServiceImpl.java
@@ -67,180 +67,190 @@ import static org.smallbun.framework.security.SecurityHandler.USER;
  */
 @Service
 public class SysUserServiceImpl extends BaseServiceImpl<SysUserMapper, SysUserEntity>
-		implements SysUserService, LoginSuccessHandler {
+        implements SysUserService, LoginSuccessHandler {
 
 
-	@Autowired
-	public SysUserServiceImpl(SysRoleService sysRoleService, SmallBunProperties smallBunProperties) {
-		this.sysRoleService = sysRoleService;
-		this.smallBunProperties = smallBunProperties;
-	}
+    @Autowired
+    public SysUserServiceImpl(SysRoleService sysRoleService, SmallBunProperties smallBunProperties) {
+        this.sysRoleService = sysRoleService;
+        this.smallBunProperties = smallBunProperties;
+    }
 
-	/**
-	 * model
-	 * @param request
-	 * @return
-	 */
-	@Override
-	public SysUserVO model(HttpServletRequest request) {
-		if (!request.getRequestURI().contains(UNIQUE)) {
-			return StringUtils.isEmpty(request.getParameter(ID)) ?
-					new SysUserVO() :
-					AutoMapperUtil.mapping(getById(request.getParameter(ID)), new SysUserVO());
-		}
-		return new SysUserVO();
-	}
+    /**
+     * model
+     *
+     * @param request
+     * @return
+     */
+    @Override
+    public SysUserVO model(HttpServletRequest request) {
+        if (!request.getRequestURI().contains(UNIQUE)) {
+            return StringUtils.isEmpty(request.getParameter(ID)) ?
+                    new SysUserVO() :
+                    AutoMapperUtil.mapping(getById(request.getParameter(ID)), new SysUserVO());
+        }
+        return new SysUserVO();
+    }
 
-	/**
-	 * 重写getById
-	 * @param id
-	 * @return
-	 */
-	@Override
-	public SysUserEntity getById(Serializable id) {
-		return baseMapper.findById(id);
-	}
+    /**
+     * 重写getById
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public SysUserEntity getById(Serializable id) {
+        return baseMapper.findById(id);
+    }
 
-	/**
-	 * 保存或更新
-	 * @param vo
-	 * @return
-	 */
-	@Override
-	public boolean saveOrUpdate(SysUserVO vo) {
-		boolean flag;
-		//如果没有密码，使用默认密码
-		if (StringUtils.isEmpty(vo.getPassword())) {
-			vo.setPassword(smallBunProperties.getUser().getRegisterDefaultPassword());
-		}
-		flag = super.saveOrUpdate(vo);
-		if (flag) {
-			//根据用户删除关联,保存用户和角色
-			sysRoleService.delRoleUserByUserId(vo.getId());
-			sysRoleService.saveRoleUser(vo.getId().toString(),
-					vo.getRoleVOS().stream().filter(u -> !StringUtils.isEmpty(u.getId())).map(SysRoleEntity::getId)
-							.map(Objects::toString).collect(Collectors.toList()));
-		}
-		return flag;
-	}
+    /**
+     * 保存或更新
+     *
+     * @param vo
+     * @return
+     */
+    @Override
+    public boolean saveOrUpdate(SysUserVO vo) {
+        boolean flag;
+        //如果为添加，使用默认密码
+        if (StringUtils.isEmpty(vo.getId()) && StringUtils.isEmpty(vo.getPassword())) {
+            vo.setPassword(smallBunProperties.getUser().getRegisterDefaultPassword());
+        }
+        flag = super.saveOrUpdate(vo);
+        if (flag) {
+            //根据用户删除关联,保存用户和角色
+            sysRoleService.delRoleUserByUserId(vo.getId());
+            sysRoleService.saveRoleUser(vo.getId().toString(),
+                    vo.getRoleVOS().stream().filter(u -> !StringUtils.isEmpty(u.getId())).map(SysRoleEntity::getId)
+                            .map(Objects::toString).collect(Collectors.toList()));
+        }
+        return flag;
+    }
 
-	/**
-	 * 根据用户名查询用户
-	 * @param username username
-	 * @return SysUserEntity
-	 */
-	@Override
-	public SysUserEntity findByUsername(String username) {
-		return baseMapper.findByUsername(username);
-	}
+    /**
+     * 根据用户名查询用户
+     *
+     * @param username username
+     * @return SysUserEntity
+     */
+    @Override
+    public SysUserEntity findByUsername(String username) {
+        return baseMapper.findByUsername(username);
+    }
 
-	/**
-	 * 自定义分页查询
-	 * @param page
-	 * @param vo
-	 * @return
-	 */
-	@Override
-	public IPage<SysUserEntity> page(Page<SysUserEntity> page, SysUserVO vo) {
-		return baseMapper.page(page, vo);
-	}
+    /**
+     * 自定义分页查询
+     *
+     * @param page
+     * @param vo
+     * @return
+     */
+    @Override
+    public IPage<SysUserEntity> page(Page<SysUserEntity> page, SysUserVO vo) {
+        return baseMapper.page(page, vo);
+    }
 
-	/**
-	 *修改密码
-	 * @param oldPassword 旧密码
-	 * @param newPassword 新密码
-	 * @param confirmPassword 确认密码
-	 * @return
-	 */
-	@Override
-	public boolean changePassword(String oldPassword, String newPassword, String confirmPassword) {
-		//1.判断旧密码
-		if (!new BCryptPasswordEncoder().matches(oldPassword, Objects.requireNonNull(getLoginUser()).getPassword())) {
-			throw new BusinessExecption(EX000102, "旧密码输入错误");
-		}
-		//2.新密码
-		if (!newPassword.equals(confirmPassword)) {
-			throw new BusinessExecption("两次输入密码不一致");
-		}
-		//修改密码
-		int i = baseMapper
-				.changePassword(getLoginUser().getSysUser().getId(), new BCryptPasswordEncoder().encode(newPassword));
-		return i > 0;
-	}
+    /**
+     * 修改密码
+     *
+     * @param oldPassword     旧密码
+     * @param newPassword     新密码
+     * @param confirmPassword 确认密码
+     * @return
+     */
+    @Override
+    public boolean changePassword(String oldPassword, String newPassword, String confirmPassword) {
+        //1.判断旧密码
+        if (!new BCryptPasswordEncoder().matches(oldPassword, Objects.requireNonNull(getLoginUser()).getPassword())) {
+            throw new BusinessExecption(EX000102, "旧密码输入错误");
+        }
+        //2.新密码
+        if (!newPassword.equals(confirmPassword)) {
+            throw new BusinessExecption("两次输入密码不一致");
+        }
+        //修改密码
+        int i = baseMapper
+                .changePassword(getLoginUser().getSysUser().getId(), new BCryptPasswordEncoder().encode(newPassword));
+        return i > 0;
+    }
 
-	/**
-	 * 验证原密码
-	 * @param oldPassword
-	 * @return
-	 */
-	@Override
-	public boolean verifyOldPassword(String oldPassword) {
-		return new BCryptPasswordEncoder().matches(oldPassword, Objects.requireNonNull(getLoginUser()).getPassword());
-	}
+    /**
+     * 验证原密码
+     *
+     * @param oldPassword
+     * @return
+     */
+    @Override
+    public boolean verifyOldPassword(String oldPassword) {
+        return new BCryptPasswordEncoder().matches(oldPassword, Objects.requireNonNull(getLoginUser()).getPassword());
+    }
 
-	/**
-	 * 更新头像
-	 * @param id
-	 * @param url
-	 * @return
-	 */
-	@Override
-	public boolean updateHeadPortrait(String id, String url) {
-		boolean b = baseMapper.updateHeadPortrait(id, url);
-		//更新用户头像
-		if (b) {
-			//更新LoginUserDetails
-			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-			UsernamePasswordAuthenticationToken newAuth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(),
-					auth.getCredentials(), auth.getAuthorities());
-			//设置setDetails
-			LoginUserDetails details = (LoginUserDetails) auth.getDetails();
-			details.getSysUser().setHeadPortraitUrl(url);
-			newAuth.setDetails(details);
-			SecurityContextHolder.getContext().setAuthentication(newAuth);
-		}
-		return b;
-	}
+    /**
+     * 更新头像
+     *
+     * @param id
+     * @param url
+     * @return
+     */
+    @Override
+    public boolean updateHeadPortrait(String id, String url) {
+        boolean b = baseMapper.updateHeadPortrait(id, url);
+        //更新用户头像
+        if (b) {
+            //更新LoginUserDetails
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            UsernamePasswordAuthenticationToken newAuth = new UsernamePasswordAuthenticationToken(auth.getPrincipal(),
+                    auth.getCredentials(), auth.getAuthorities());
+            //设置setDetails
+            LoginUserDetails details = (LoginUserDetails) auth.getDetails();
+            details.getSysUser().setHeadPortraitUrl(url);
+            newAuth.setDetails(details);
+            SecurityContextHolder.getContext().setAuthentication(newAuth);
+        }
+        return b;
+    }
 
-	/**
-	 * changPassword
-	 * @param password
-	 * @return
-	 */
-	@Override
-	public boolean changPassword(String password) {
-		//修改密码
-		int i = baseMapper.changePassword(Objects.requireNonNull(getLoginUser()).getSysUser().getId(),
-				new BCryptPasswordEncoder().encode(password));
-		return i > 0;
-	}
+    /**
+     * changPassword
+     *
+     * @param password
+     * @return
+     */
+    @Override
+    public boolean changPassword(String password) {
+        //修改密码
+        int i = baseMapper.changePassword(Objects.requireNonNull(getLoginUser()).getSysUser().getId(),
+                new BCryptPasswordEncoder().encode(password));
+        return i > 0;
+    }
 
 
-	/**
-	 * 修改登录信息
-	 * @param userDetails {@link UserDetails}
-	 */
-	@Override
-	public void updateLoginInfo(UserDetails userDetails) {
-		LoggedUserBindingListener activeUserStore = (LoggedUserBindingListener) UserUtil.getSession()
-				.getAttribute(USER);
-		LoggedUser loggedUser = activeUserStore.getLoggedUser();
-		//强转LoginUserDetails
-		LoginUserDetails user = (LoginUserDetails) userDetails;
-		//设置ip
-		user.getSysUser().setLastLoginIp(loggedUser.getLogInIp());
-		//设置地址
-		user.getSysUser().setLastLoginAddress(loggedUser.getLogInAddress());
-		//设置登录时间
-		user.getSysUser().setLastLoginTime(new Timestamp(loggedUser.getLogInTime().getTime()));
-		//调用修改
-		baseMapper.updateLastLoginInfo(user.getSysUser());
-	}
+    /**
+     * 修改登录信息
+     *
+     * @param userDetails {@link UserDetails}
+     */
+    @Override
+    public void updateLoginInfo(UserDetails userDetails) {
+        LoggedUserBindingListener activeUserStore = (LoggedUserBindingListener) UserUtil.getSession()
+                .getAttribute(USER);
+        LoggedUser loggedUser = activeUserStore.getLoggedUser();
+        //强转LoginUserDetails
+        LoginUserDetails user = (LoginUserDetails) userDetails;
+        //设置ip
+        user.getSysUser().setLastLoginIp(loggedUser.getLogInIp());
+        //设置地址
+        user.getSysUser().setLastLoginAddress(loggedUser.getLogInAddress());
+        //设置登录时间
+        user.getSysUser().setLastLoginTime(new Timestamp(loggedUser.getLogInTime().getTime()));
+        //调用修改
+        baseMapper.updateLastLoginInfo(user.getSysUser());
+    }
 
-	/**
-	 * 注入角色业务逻辑接口
-	 */
-	private final SysRoleService sysRoleService;
+    /**
+     * 注入角色业务逻辑接口
+     */
+    private final SysRoleService sysRoleService;
 
-	private final SmallBunProperties smallBunProperties;
+    private final SmallBunProperties smallBunProperties;
 }
