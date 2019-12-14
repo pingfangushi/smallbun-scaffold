566cea2c9fa83c2973d206ee8226dc870d1c63c3
SanLi <2689170096@qq.com>
2019-12-14 18:45:51 +0800

    :zap: 优化代码

--- src/main/java/cn/smallbun/scaffold/manage/pojo/PortVO.java
+++ src/main/java/cn/smallbun/scaffold/manage/pojo/PortVO.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019.  ‭‭‭‭‭‭‭‭‭‭‭‭[zuoqinggang] www.pingfangushi.com
+ * Copyright (c) 2018-2019. ‭‭‭‭‭‭‭‭‭‭‭‭[zuoqinggang] www.pingfangushi.com
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
